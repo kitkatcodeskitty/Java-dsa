0f0c29ed758833cdf261325c14c30b2fe6410e2b
dilan dhakal <dilandhakal889@gmail.com>
2025-12-10 13:36:25 +0545

    first commit

---
+++ first.java
@@ -0,0 +1,17 @@
+
+// import java.util.*;
+
+// public class firstClass {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String firstName = sc.next();
+//         String lastName = sc.next();
+//         System.out.println("name is " + firstName + lastName);
+
+//     }
+// }
+
+
+public class firstClass {
+    public static vo
+}
---
+++ firstClass.java
@@ -0,0 +1,64 @@
+
+    // import java.util.*;
+
+    // public class firstClass {
+    //     public static void main(String[] args){
+    //         Scanner sc = new Scanner(System.in);
+    //         String firstName = sc.next();
+    //         String lastName = sc.next();
+    //         System.out.println("name is " + firstName + lastName);
+
+    //     }
+    // }
+
+    // import java.util.*;
+    // public class firstClass {
+    //     public static void main(String[] args) {
+    //         Scanner sc = new Scanner(System.in);
+    //         int age = sc.nextInt();
+
+    //         if(age> 18){
+    //             System.out.println("adult");
+
+    //         }
+    //         else{
+    //             System.out.println("not adult");
+    //         }
+    //     }
+    // }
+
+
+
+    // import java.util.*;
+    // class firstClass {
+    //     public static void main(String[] args){
+    //         int p = 10000;
+    //         int t = 2;
+    //         int r = 4;
+    //         int simpleInterest = p * t * r;
+    //         int SimpleInterestTotal = simpleInterest/100;
+    //         System.out.println("Simple interest=" + SimpleInterestTotal);
+    //     }
+    // }
+
+
+    // import java.util.*;
+
+    // class firstClass {
+    //     public static void main(String[] args){
+    //         float pi = 22/7;
+    //         float radius = 10;
+    //         float AreaOfCircle = pi * radius * radius;
+    //         System.out.println("Area of circle  = " + AreaOfCircle);
+    //     }
+    // }
+
+// class firstClass {
+//     public static void main (String[] args){
+//         int var1 = 5;
+//         int var2 = 6;
+//         int var3;
+//         var3=++var2*var1/var2+var2;
+//         System.out.println(var3);
+//     }
+// }
